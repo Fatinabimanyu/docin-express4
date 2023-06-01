e4c7d4673f441d097918318a757c8f96af667850
fatinabimanyu <fatinabimanyu@gmail.com>
2023-06-01 21:27:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-import process from 'process';
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import usersRouter from './src/routes/usersRoute.js';
-import doctorsRouter from './src/routes/doctorsRoute.js';
-import appointmentsRouter from './src/routes/appointmentsRoute.js';
+import process from "process";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import usersRouter from "./src/routes/usersRoute.js";
+import doctorsRouter from "./src/routes/doctorsRoute.js";
+import appointmentsRouter from "./src/routes/appointmentsRoute.js";
 
-import getenv from './src/helper/getenv.js';
+import getenv from "./src/helper/getenv.js";
 
 const app = express();
 
-const PORT = getenv('PORT');
-const MONGO_URI = getenv('MONGO_URI');
+const PORT = getenv("PORT");
+const MONGO_URI = getenv("MONGO_URI");
 
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log('Connected to DocIn Database!'))
+  .then(() => console.log("Connected to DocIn Database!"))
   .catch((err) => {
     console.error(`Can't connect to DocIn Database!`);
     console.error(err);
     process.exit(1);
   });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Express');
+app.get("/", (req, res) => {
+  res.send("Express");
 });
 
-app.use('/users', usersRouter);
-app.use('/doctors', doctorsRouter);
-app.use('/appointments', appointmentsRouter);
+app.use("/users", usersRouter);
+app.use("/doctors", doctorsRouter);
+app.use("/appointments", appointmentsRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
